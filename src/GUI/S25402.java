74f254162fe1088a31464c6cb07ec8bcfcb0190a
Mpawelo <milohero@wp.pl>
2023-06-07 13:55:42 +0200

    Call GUI

---
+++ src/GUI/S25402.java
@@ -0,0 +1,10 @@
+package GUI;
+
+public class S25402 {
+
+    public S25402() {
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+    }
+    //Loginy i hasła: user/user, foreman/foreman
+}
